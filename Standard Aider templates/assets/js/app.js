50dfc3321470fbf36e74a17c1e3b54aac3729944
Umar Muhammad Zakari <Umarfarouqft@gmail.com>
2020-09-25 14:57:04 +0200

    Bug fixes

--- Standard Aider templates/assets/js/app.js
+++ Standard Aider templates/assets/js/app.js
@@ -43,7 +43,7 @@ function changeTemplate(toggleType, toggleValue) {
                 $('#page').removeClass('roboto').removeClass('ubuntu').removeClass('monospace').addClass('cursive');
             else if (toggleValue == 'Ubuntu')
                 $('#page').removeClass('roboto').removeClass('monospace').removeClass('cursive').addClass('ubuntu');
-            else if (toggleValue == 'monospace')
+            else if (toggleValue == 'Monospace')
                 $('#page').removeClass('ubuntu').removeClass('cursive').removeClass('roboto').addClass('monospace');
             break;
         case 'case':
@@ -208,72 +208,3 @@ function insertAfter(referenceNode, newNode) {
 }
 
 
-
-// Navbar
-
-function toggleClass(elem,className){
-    if (elem.className.indexOf(className) !== -1){
-      elem.className = elem.className.replace(className,'');
-    }
-    else{
-      elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
-    }
-  
-    return elem;
-  }
-  
-  function toggleDisplay(elem){
-    const curDisplayStyle = elem.style.display;			
-  
-    if (curDisplayStyle === 'none' || curDisplayStyle === ''){
-      elem.style.display = 'block';
-    }
-    else{
-      elem.style.display = 'none';
-    }
-  
-  }
-  
-  function toggleMenuDisplay(e){
-    const dropdown = e.currentTarget.parentNode;
-    const menu = dropdown.querySelector('.menu');
-    const icon = dropdown.querySelector('.fa-angle-right');
-  
-    toggleClass(menu,'hide');
-    toggleClass(icon,'rotate-90');
-  }
-  
-  function handleOptionSelected(e){
-    toggleClass(e.target.parentNode, 'hide');			
-  
-    const id = e.target.id;
-    const newValue = e.target.textContent + ' ';
-    const titleElem = document.querySelector('.dropdown .title');
-    const icon = document.querySelector('.dropdown .title .fa');
-  
-  
-    titleElem.textContent = newValue;
-    titleElem.appendChild(icon);
-  
-    //trigger custom event
-    document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
-      //setTimeout is used so transition is properly shown
-    setTimeout(() => toggleClass(icon,'rotate-90',0));
-  }
-  
-  function handleTitleChange(e){
-    const result = document.getElementById('result');
-  
-    result.innerHTML = 'The result is: ' + e.target.textContent;
-  }
-  
-  //get elements
-  const dropdownTitle = document.querySelector('.dropdown .title');
-  const dropdownOptions = document.querySelectorAll('.dropdown .option');
-  
-  //bind listeners to these elements
-  dropdownTitle.addEventListener('click', toggleMenuDisplay);
-  
-  dropdownOptions.forEach(option => option.addEventListener('click',handleOptionSelected));
-  
-  document.querySelector('.dropdown .title').addEventListener('change',handleTitleChange);
